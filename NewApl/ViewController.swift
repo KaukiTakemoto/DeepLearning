95fe4e6f54a46e1201485e31ceefc491d9de7466
竹本和樹 <Kazuki.Takemoto@gmail.com>
2016-08-05 22:42:39 +0900

    Initial Commit

---
+++ NewApl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewApl
+//
+//  Created by 竹本和樹 on 2016/08/05.
+//  Copyright © 2016年 竹本和樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
